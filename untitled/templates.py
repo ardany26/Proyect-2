b04d209d1e8ef72fc5a3c4892e27df96abf623a4
ardany26 <mmmardny@gmail.com>
2017-11-14 23:24:20 -0600

    registro
    registro e intento de chat

--- untitled/templates.py
+++ untitled/templates.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import request, g, redirect, url_for, render_template, flash
+from flask import request, redirect, url_for, render_template, flash
 
 app = Flask(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
@@ -26,10 +26,15 @@ while(x < len(psw)):
     contraseñas = contraseñas + contraseña.split(" ")
 print(contraseñas)
 
+Mensajes = []
+contador = 0
+
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    global usuarios, contraseñas
+
+    global usuario, usuarios, contraseñas, contador
+    
     error = None
     if(request.method == 'POST'):
         if (request.form['entrar'] == "Entrar"):
@@ -37,28 +42,86 @@ def index():
                 usuario = (request.form['nombre'],request.form['pswd'])
                 i = 0
                 while(i < len(usuarios)):
+                    contador = i
                     if(usuario[0] == usuarios[i] and usuario[1] == contraseñas[i]):
-                        return redirect(url_for('chat'))
+                        return redirect(url_for('who'))
                     i = i + 1
                 else: 
-                    error = "Usuario o contraseña incorrectos, intente de nuevo"
+                    error = "Usuario y/o contraseña incorrectos"
             else:
                 error = "No ha introducido datos"
 
     return render_template('index.html', error=error)
 
-@app.route('/Registro')
+@app.route('/Registro', methods=['GET','POST'])
 def registro():
-    global usuarios, contraseñas
+
+    global usuario, pswd, usuarios, contraseñas
+
+    if(request.method == "POST"):
+        usuario = request.form['nombre']
+        pswd = request.form['pswd']
+        f = open("usuarios.txt", "r")
+        texto = f.readlines()
+        f.close()
+        usuarios = (texto[0]).split()
+        g = open("contraseñas.txt", "r")
+        texto = g.readlines()
+        g.close()
+        contraseñas = (texto[0]).split()
+        if not(usuario in usuarios):
+            f = open("usuarios.txt", "a")
+            f.write(" " + usuario + " ")
+            f.close()
+            if not(pswd in contraseñas):
+                g = open("contraseñas.txt", "a")
+                g.write(" " + pswd + " ")
+                g.close()
+                return redirect(url_for('chat'))
+
     return render_template('registro.html')
 
-@app.route('/Chat')
+@app.route('/Who', methods=['GET','POST'])
+def who():
+    
+    global usuarios, Mensajes, chat
+
+    error = None
+    if(request.method == "POST"):
+        if(request.form['abrir'] == "Abrir conversación"):
+            if(request.form['who'] != ""):
+                chat = request.form['who']
+                i = 0
+                while(i < len(usuarios)):
+                    if(chat == usuarios[i]):
+                        Mensajes.append([])
+                        return redirect(url_for('chat'))
+                    i = i + 1
+                else:
+                    error = "Ingrese Nombre de usuario"
+
+    return render_template('Who.html', error=error)
+
+@app.route('/Chat', methods=['GET','POST'])
 def chat():
-    global usuarios, contraseñas
-    return render_template('Chat.html')
+    
+    global usuarios, contraseñas, Mensajes, contador, chat
+
+    if(request.method == 'POST'): 
+        if(request.form['mensaje'] != ""):
+            mensaje = (chat, request.form['mensaje'])
+            i = 0
+            while(i < len(usuarios)):
+                if(usuarios[i] == mensaje[1]):
+                    Mensajes.append(request.form['mensaje'])
+                i = i + 1
+
+    entries = {'datos1' : request.form["nombre"], 'datos2' : Mensajes[contador], 'datos3' : chat}
+    
+    return render_template('Chat.html', entries=entries)
         
 
 if __name__ == '__main__':
-    app.run(debug = True, port=9000)
+    app.run(debug=True, port=9000)
  
 
